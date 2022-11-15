9189814336c32c3918035bb8b4ce3049fb3c1334
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2022-11-15 15:28:50 -0300

    visual
    pagina do aluno,registrar,login,editar

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,6 +2,39 @@
 
 @section('content')
 
+<style>
+body{
+
+background-image: url(https://thumbs.dreamstime.com/b/dark-technology-background-vector-big-data-purple-digital-connection-high-wireless-texture-visualization-screen-wireframe-160622718.jpg);
+}
+
+
+
+.navbar-brand img {
+width: 50px;
+margin-left: -3rem;
+}
+
+.btn-primary{
+background-color: #2a251f;
+border: 2px solid #7f0072;
+}
+
+
+.btn-primary:hover{
+background-color: #aa05a2;
+border: 2px solid #171614;
+}
+
+h1 {
+    color: aliceblue;
+
+}
+.form-group{
+    color: aliceblue; 
+}
+</style>
+
 <div id="edit-create-container" class="col-md-6 offset-md-3">
     <h1>Edite as informações</h1>
     <form action="/home/update/{{Auth::user()->id}}" method="POST">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'X Project') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -16,12 +17,15 @@
     <!-- Scripts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
     <link rel="stylesheet" href="css/styles.css">
- 
+    <style>
+   
+     </style>
 </head>
+
 <body>
     <div id="app">
+
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -77,6 +81,7 @@
         </nav>
 
         <main class="py-4">
+        
             @yield('content')
         </main>
     </div>
