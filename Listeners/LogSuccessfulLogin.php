f298fb4ac5a7b6588eb21a180bb1f7519b18842b
xF3rd <102755227+xF3rd@users.noreply.github.com>
2022-11-26 21:12:40 -0300

    Fiz o ultimo login
    composer require nesbot/carbon

---
+++ Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Carbon\Carbon;
+
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+       //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \Illuminate\Auth\Events\Login  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        $event->user->ultimo_login = Carbon::now();
+        $event->user->save();
+    }
+}
